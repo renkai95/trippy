214994a08f4171fe3a55156ca0016d5c4973f63d
rk <rk@rks-iMac.local>
2019-05-02 16:07:56 +1000

    Initial Commit

---
+++ trippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trippy
+//
+//  Created by rk on 2/5/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
